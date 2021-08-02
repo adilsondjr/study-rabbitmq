9b5caceacfa484b94819c39811ccad9c301a6d99
Adilson Domiciano Jr <adilsondjr@inatel.br>
2021-08-02 15:47:14 -0300

    Create queues ESTOQUE and PRECO

---
+++ estoquepreco/src/main/java/br/com/adilsondjr/ms/estoquepreco/connections/RabbitMQConection.java
@@ -0,0 +1,54 @@
+package br.com.adilsondjr.ms.estoquepreco.connections;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class RabbitMQConection {
+    private static final String EXCHANGE_NAME = "amq.direct";
+    private static final String QUEUE_ESTOQUE = "ESTOQUE";
+    private static final String QUEUE_PRECO = "PRECO";
+
+    private AmqpAdmin amqpAdmin;
+
+    public RabbitMQConection(AmqpAdmin amqpAdmin) {
+        this.amqpAdmin = amqpAdmin;
+    }
+
+    private Queue queue(String queueName) {
+        return new Queue(queueName, true, false, false);
+    }
+
+    private DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    private Binding related(Queue queue, DirectExchange exchange) {
+        return new Binding(queue.getName(), Binding.DestinationType.QUEUE, exchange.getName(),
+                queue.getName(), null);
+    }
+
+    @PostConstruct
+    private void addQueue() {
+        Queue queueEstoque = this.queue(QUEUE_ESTOQUE);
+        Queue queuePreco = this.queue(QUEUE_PRECO);
+
+        DirectExchange exchange = this.exchange();
+
+        Binding relatedEstoque = this.related(queueEstoque, exchange);
+        Binding relatedPreco = this.related(queuePreco, exchange);
+
+        this.amqpAdmin.declareQueue(queueEstoque);
+        this.amqpAdmin.declareQueue(queuePreco);
+
+        this.amqpAdmin.declareExchange(exchange);
+
+        this.amqpAdmin.declareBinding(relatedEstoque);
+        this.amqpAdmin.declareBinding(relatedPreco);
+    }
+}
