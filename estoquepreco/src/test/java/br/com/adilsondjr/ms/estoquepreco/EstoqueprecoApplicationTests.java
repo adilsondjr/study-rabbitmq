1daca5535b1f54355242094d162a8f39b59c76a2
Adilson Domiciano Jr <adilsondjr@inatel.br>
2021-08-02 13:54:23 -0300

    Create MS price and configure rabbitmq

---
+++ estoquepreco/src/test/java/br/com/adilsondjr/ms/estoquepreco/EstoqueprecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adilsondjr.ms.estoquepreco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueprecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
